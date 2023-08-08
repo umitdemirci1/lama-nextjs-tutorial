89e1c382b47769685571d0516b90d331540c3c25
umitd111 <umit.demirci1996@gmail.com>
2023-08-08 21:04:27 +0300

    Client side data fetching is implemented. Swr is installed

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,23 @@
-import styles from './page.module.css'
+"use client";
+
+import styles from "./page.module.css";
+import useSWR from "swr";
 
 const Dashboard = () => {
-    return ( 
-        <div className={styles.container}>
-            Dashboard
-        </div>
-     );
-}
- 
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  console.log(data);
+  return (
+    <div className={styles.container}>
+      {data.map((item) => (
+        <li key={item.id}>{item.title}</li>
+      ))}
+    </div>
+  );
+};
+
 export default Dashboard;
