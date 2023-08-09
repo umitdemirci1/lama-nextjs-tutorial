019e9d690e2c774be347cc6712b21b1e436a024e
umitd111 <umit.demirci1996@gmail.com>
2023-08-09 19:12:06 +0300

    Revert "Mongoose 6.11.1 installed. DB connection is established"
    This reverts commit acf9ba0d1d49b3909cb8203c7f2671666783998c.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
     images: {
         domains: ["images.pexels.com"]
-    },
-    env: {
-        MONGO: "mongodb+srv://umitdemirci1996:qWcnz6OPsE8FMfgP@lama-dev-next.7ajp7h4.mongodb.net/?retryWrites=true&w=majority"
     }
 }
 
