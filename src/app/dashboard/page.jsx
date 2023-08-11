c07d9619020ad08f11ad922133db4187861dfe62
umitd111 <umit.demirci1996@gmail.com>
2023-08-11 23:18:01 +0300

    Dashboard is encapsulated from not logged in

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,6 +5,8 @@ import styles from "./page.module.css";
 import useSWR from "swr";
 
 const Dashboard = () => {
+  const session = useSession()
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
     "https://jsonplaceholder.typicode.com/posts",
@@ -16,6 +18,7 @@ const Dashboard = () => {
   }
   return (
     <div className={styles.container}>
+      {session.status==="authenticated" && <div> Giris yapildi </div>}
       {data.map((item) => (
         <li key={item.id}>{item.title}</li>
       ))}
